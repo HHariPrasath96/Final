4f415401182f13b1232afe10d5d96a7c30f0418e
HHariprasath96 <hariprasathalagesan@gmail.com>
2022-11-07 19:14:24 +0530

    my task

---
+++ src/org/sample/Case1.java
@@ -0,0 +1,31 @@
+package org.sample;
+
+public class Case1 {
+	
+	
+	private void method() {
+           System.out.println("c#");
+	}
+	
+	private void method2() {
+           System.out.println("java");
+	}
+	
+	private void method3() {
+           System.out.println("Pithon");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
